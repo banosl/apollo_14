99b82035abb47ccb38b3381478bc7187aa6e957c
Leo Banos Garcia <bsleo625@gmail.com>
2022-12-29 14:40:17 -0700

    Establishes relationships in models

---
+++ spec/features/astronauts/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Astronauts Index", type: :feature do
+  describe 'as a user' do
+    describe 'when I visit /astronauts' do
+      it 'see a list of astronauts with name, age, job' do
+
+      end
+
+      it 'see average age of all astronauts'
+
+      it 'see list of space missions in alphabetical order for each astronaut'
+
+      it 'see the total time in space for each astronaut'
+    end
+  end
+end
